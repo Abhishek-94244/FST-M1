deb9ad22789cc6e33a979df30fbd682f3f9bdd7c
Abhishek-94244 <66135318+Abhishek-94244@users.noreply.github.com>
2022-11-08 12:14:43 +0530

    Uploaded

---
+++ JUnit/Activities/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+
+    public int multiply(int i, int i1) {
+           int e = i ;
+           int v = i1 ;
+           int c = e*v ;
+        return c;
+    }
+}
---
+++ JUnit/Activities/randomeJUnite.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class randomeJUnite {
+    private static StringBuilder output = new StringBuilder("");
+
+   /* @Test
+    void myATest() {
+        output.append("A");
+    }
+
+    @Test
+    void myBTest() {
+        output.append("B");
+    }
+
+    @Test
+    void myCTest() {
+        output.append("C");
+    }*/
+
+    @Test
+    void test1() {
+        System.out.println("This is test case 1");
+    }
+
+    @Test
+    void test2() {
+        System.out.println("This is test case 2");
+    }
+
+    @Test
+    void test3() {
+        System.out.println("This is test case 3");
+    }
+   /* @AfterAll
+    public static void assertOutput() {
+        assertEquals("ACB", output.toString());
+    }*/
+}
