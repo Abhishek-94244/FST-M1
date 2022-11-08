bd605d27f90b28abb2f0a70573631f7aefd16962
Abhishek-94244 <66135318+Abhishek-94244@users.noreply.github.com>
2022-11-08 10:30:27 +0530

    Uploaded

---
+++ Selenium/Activities/Activity8_3.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity8_3 {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+
+
+    }
+}
